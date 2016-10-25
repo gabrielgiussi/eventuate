ae44e5c02b76e422c15dae02a6b82b8edb0c0ff7
Gabriel Giussi <gabrielgiussi@gmail.com>
2016-10-24 22:56:13 -0300

    Added CERMatch (based on OrSet). Untested

---
+++ eventuate-crdt/src/main/scala/com/rbmhtechnology/eventuate/crdt/CERMatch.scala
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2015 - 2016 Red Bull Media House GmbH <http://www.redbullmediahouse.com> - all rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.rbmhtechnology.eventuate.crdt
+
+import java.util.Date
+
+import com.rbmhtechnology.eventuate.{ Versioned, DurableEvent, VectorTime }
+
+import scala.collection.immutable.Set
+
+case class CERMatch(date: Date = null, orSet: ORSet[String] = ORSet.apply[String]) extends CRDTFormat {
+
+  def value: Match = Match(date, orSet.value)
+
+  def add(player: String, timestamp: VectorTime): (CERMatch, Option[Versioned[String]]) = {
+    val updatedOrSet = orSet.add(player, timestamp)
+    if (orSet.value.size < CERMatch.MATCH_SIZE)
+      (copy(date, updatedOrSet), None)
+    else {
+      val removed = updatedOrSet.versionedEntries.toVector.sorted(CERMatch.CERMatchOrdering).last
+      (copy(date, updatedOrSet.remove(updatedOrSet.prepareRemove(removed.value))), Some(removed))
+    }
+  }
+
+  def prepareRemove(entry: String): Set[VectorTime] = orSet.prepareRemove(entry)
+
+  def remove(timestamps: Set[VectorTime]): CERMatch = copy(date, orSet.remove(timestamps))
+}
+
+case class Match(date: Date, players: Set[String] = Set.empty)
+
+object CERMatch {
+  def apply(): CERMatch =
+    new CERMatch()
+
+  val MATCH_SIZE = 3
+
+  val orSetOps = ORSet.ORSetServiceOps[String]
+
+  implicit def CERMatchOrdering = new Ordering[Versioned[String]] {
+    override def compare(x: Versioned[String], y: Versioned[String]): Int =
+      if (x.systemTimestamp == y.systemTimestamp)
+        x.value.toString.compareTo(y.value.toString)
+      //x.creator.compareTo(y.creator)
+      else
+        x.systemTimestamp.compareTo(y.systemTimestamp)
+  }
+
+  implicit def CERMatchServiceOps = new CRDTServiceOps[CERMatch, Match] {
+    override def zero: CERMatch =
+      CERMatch.apply()
+
+    override def value(crdt: CERMatch): Match = crdt.value
+
+    override def prepare(crdt: CERMatch, operation: Any): Option[Any] = operation match {
+      //case op @ RemoveOp(entry, _) => ??
+      case op @ AddOp(e) if (crdt.value.players.size < MATCH_SIZE) => orSetOps.prepare(crdt.orSet, operation)
+      case _ => None
+    }
+
+    override def effect(crdt: CERMatch, operation: Any, event: DurableEvent): (CERMatch, Option[Apology]) = operation match {
+      case RemoveOp(_, timestamps) => (crdt.remove(timestamps), None)
+      case AddOp(entry) =>
+        crdt.add(entry.asInstanceOf[String], event.vectorTimestamp) match {
+          case (m, Some(removed)) => (m, Some(Apology(removed, entry)))
+          case (m, None)          => (m, None)
+        }
+    }
+  }
+
+}
